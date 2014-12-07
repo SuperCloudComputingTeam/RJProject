75fd2d0a82b0f4bb6616c195185c6e85fd26aeea
YiLingPredator <lingyi0530@gmail.com>
2014-12-07 03:10:43 -0600

    generic case for partition
    almost finish generic case of partition for any reducers

---
+++ RJProject/src/com/company/ReducerRegion.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 12/7/14.
+ */
+public class ReducerRegion {
+    //Each ReducerRegion will only have a S_range, a T_range and it's own reducer id
+    Range S_range;
+    Range T_range;
+    int reducersID;
+
+    public ReducerRegion(){
+        S_range = new Range();
+        T_range = new Range();
+        reducersID = 0;
+    }
+
+    public ReducerRegion(Range s, Range t, int id){
+        S_range = s;
+        T_range = t;
+        reducersID = id;
+    }
+}
--- RJProject/src/com/company/Test.java
+++ RJProject/src/com/company/Test.java
@@ -5,25 +5,30 @@ import java.util.ArrayList;
 /**
  * Created by Alex on 12/6/14.
  */
-public class Test {
-    public static void main(String[] args) throws Exception {
-        //The code below is pulled from my testing project. So change accordingly.
+public class Test
+{
+    public ArrayList<ReducerRegion> Reducers = new ArrayList<ReducerRegion>();
+
+    public  void Test(double S, double T, double r) throws Exception {
         //Assume |S|<|T|
         //int numReduers = Integer.parseInt(args[0]);
         //int sSize = Integer.parseInt(args[1]);
         //int tSize = Integer.parseInt(args[2]);
-        double numReduers =5;
-        double sSize=2;
-        double tSize=13;
+
+        double numReduers =r;
+        double sSize=S;
+        double tSize=T;
+
+        numReduers=5;
+        sSize=5;
+        tSize=10;
 
         double idealSquareLength = Math.sqrt((sSize*tSize)/numReduers);
-        double SquareArea = (sSize*tSize)/numReduers;
+        double idealSquareArea = (sSize*tSize)/numReduers;
 
         //calculate the Cs and Ct
         float sCoefficient = (float)(sSize/idealSquareLength);
         float tCoefficient = (float)(tSize/idealSquareLength);
-
-        /*
 
         //This is for storing all of the regions as a result from the parititoning
         ArrayList<Region> reducersRegion = new ArrayList<Region>();
@@ -34,14 +39,16 @@ public class Test {
         if (Math.floor(idealSquareLength)==idealSquareLength && sCoefficient%1 == 0 && tCoefficient%1 == 0)
         {
             //Ideal Case
+            System.out.println("Ideal Case");
 
         }
 
         else if (sSize < tSize/numReduers)
         {
+            System.out.println("Extreme Case");
             //Extreme case : since I take floor, so put the left area into last region. It is not optimal. We will improve it later.
             // get the range increment. USE FLOOR
-            double rangeIncrement=Math.floor(tSize/numReduers);
+            double rangeIncrement=Math.floor(tSize / numReduers);
             // divide into two cases.
 
             //partition S into one row
@@ -61,13 +68,131 @@ public class Test {
             }
             Region temp1=new Region(new Range((int)((numReduers-1)*rangeIncrement),(int)tSize),"T",(int)numReduers);
             TRegions.add(temp1);
-            System.out.println("Done");
+
         }
         else
         {
-            //General case
-            System.out.println("Done");
+            //Generic case
+            System.out.println("Generic Case");
+            // two cases. One is more than one reducer left, the other is only one reducer left(8*13)
+            //***********************************************CASE 1**********************************
+            double lowBoundLength = Math.floor(idealSquareLength);
+            double highBoundLength = Math.ceil(idealSquareLength);
+            int s_regionLength = -1;
+            int t_regionLength = -1;
+            int s_leftLength = -1;
+            int t_leftLength = -1;
+            int numLeftReducers = -1;
+
+            double floorDiff = Math.abs(Math.pow(lowBoundLength,2)-idealSquareArea);
+            double mediDiff = Math.abs(lowBoundLength*highBoundLength-idealSquareArea);
+            double ceilDiff = Math.abs(Math.pow(highBoundLength,2)-idealSquareArea);
+
+            //find length of each side of region(s_regionLength and t_regionLength)
+            if (Math.min(Math.min(floorDiff,mediDiff),ceilDiff) == floorDiff)
+            {
+                s_regionLength = (int)lowBoundLength;
+                t_regionLength = (int)lowBoundLength;
+            }
+            else if (Math.min(Math.min(floorDiff,mediDiff),ceilDiff) == mediDiff)
+            {
+                s_regionLength = (int)lowBoundLength;
+                t_regionLength = (int)highBoundLength;
+            }
+            else
+            {
+                s_regionLength =(int)highBoundLength;
+                t_regionLength = (int)highBoundLength;
+            }
+            numLeftReducers = (int)(numReduers-Math.floor(sCoefficient)*Math.floor(tCoefficient));
+            s_leftLength = (int)(sSize-Math.floor(sCoefficient) * s_regionLength);
+            t_leftLength = (int)(tSize-Math.floor(tCoefficient) * t_regionLength);
+
+            //Partition left area
+            double s_newRegionLength;
+            double t_newRegionLength;
+            double r_newRegion;
+            if (numLeftReducers==2)
+            {
+                Range s =new Range((int)(tSize-t_leftLength), (int)tSize);
+                //Range t =new Range(0,)
+                ReducerRegion temp = new ReducerRegion();
+
+                return;
+            }
+            else if(numLeftReducers==1)
+            {
+                System.out.print("only one reducer left, this part doesn't finished yet");
+                System.exit(0);
+                return;
+            }
+            else
+            {
+                // find the optimal cut which partitions have minimum difference
+                if (Math.min(Math.abs(s_leftLength*tSize-t_leftLength*(sSize-s_leftLength)),Math.abs(t_leftLength*sSize-s_leftLength*(tSize-t_leftLength)))==Math.abs(s_leftLength*tSize-t_leftLength*(sSize-s_leftLength)))
+                {
+                    //the bigger partition got more reducers
+                    if (s_leftLength*tSize-t_leftLength*(sSize-s_leftLength)>0)
+                    {
+                        this.Test(s_leftLength,(int)tSize,Math.ceil(numLeftReducers/2);
+                        //check which side of region should be S or T parameter
+                        if (t_leftLength<sSize-s_leftLength)
+                        {
+                            this.Test(t_leftLength,s_leftLength,numLeftReducers-Math.ceil(numLeftReducers/2);
+
+                        }
+                        else
+                        {
+                            this.Test(s_leftLength,t_leftLength,numLeftReducers-Math.ceil(numLeftReducers/2);
+                        }
+                    }
+                    else
+                    {
+                        this.Test(s_leftLength,(int)tSize,numLeftReducers-Math.ceil(numLeftReducers/2);
+                        if (t_leftLength<sSize-s_leftLength)
+                        {
+                            this.Test(t_leftLength,s_leftLength,Math.ceil(numLeftReducers/2);
+                        }
+                        else
+                        {
+                            this.Test(s_leftLength,t_leftLength,Math.ceil(numLeftReducers/2);
+                        }
+                    }
+                }
+                else//16 and 10
+                {
+                    if (t_leftLength*sSize-s_leftLength*(tSize-t_leftLength)>0)
+                    {
+                        this.Test(t_leftLength,sSize,Math.ceil(numLeftReducers/2);
+                        if (s_leftLength<tSize-t_leftLength)
+                        {
+                            this.Test(s_leftLength,tSize-t_leftLength,numLeftReducers-Math.ceil(numLeftReducers/2);
+                        }
+                        else
+                        {
+                            this.Test(tSize-t_leftLength,s_leftLength,numLeftReducers-Math.ceil(numLeftReducers/2);
+                        }
+
+                    }
+                    else
+                    {
+                        this.Test(t_leftLength,sSize,numLeftReducers-Math.ceil(numLeftReducers/2);
+                        if (s_leftLength<tSize-t_leftLength)
+                        {
+                            this.Test(s_leftLength,tSize-t_leftLength,Math.ceil(numLeftReducers/2);
+                        }
+                        else
+                        {
+                            this.Test(tSize-t_leftLength,s_leftLength,Math.ceil(numLeftReducers/2);
+                        }
+                    }
+                }
+            }
+
+
+
+
+
         }
-        */
     }
 }
